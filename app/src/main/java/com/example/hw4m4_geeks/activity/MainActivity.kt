3dd9e2f9bab522dce11a7e2a04ba667ee0d2c052
AnvarZamirov <zamirovanvar26@gmail.com>
2024-09-02 16:17:37 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw4m4_geeks/activity/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.hw4m4_geeks.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.hw4m4_geeks.R
+import com.example.hw4m4_geeks.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as
+                    NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.bottomNav.setupWithNavController(navController)
+    }
+
+}
